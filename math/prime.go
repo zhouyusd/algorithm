31efbf3929effa4fc43e4fab61adaa65c804cb1b
zhouyusd <1848611319@qq.com>
2022-09-08 01:13:32 +0800

    feat: 添加素数判断 🔥

---
+++ math/prime.go
@@ -0,0 +1,14 @@
+package math
+
+// IsPrime 判断n是否为素数
+func IsPrime(n int64) bool {
+	if n < 2 {
+		return false
+	}
+	for i := int64(2); i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
